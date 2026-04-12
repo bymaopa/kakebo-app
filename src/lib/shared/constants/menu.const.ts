a6dfbba0ae18838e6e467e28403d4f8f1aa189b7
Mario Ortega <maopadev@gmail.com>
2026-04-12 16:56:34 +0200

    fix: Se soluciona error en el visualizado del sidebar

--- src/lib/shared/constants/menu.const.ts
+++ src/lib/shared/constants/menu.const.ts
@@ -1,11 +1,25 @@
-export interface MenuLink {
-	title: string;
-	path: string;
-	icon?: string;
+/* src/lib/constants/menu.const.ts */
+
+export interface NavigationLink {
+  href: string;
+  label: string;
+  svg: string;
 }
 
-export const LIST_MENU_NAVIGATION_MOBILE = [
-		{ id: 'dashboard', label: 'Resumen', icon: 'home' },
-		{ id: 'search', label: 'Búsqueda', icon: 'search' },
-		{ id: 'user', label: 'Usuario', icon: 'user' }
-	];
+export const APP_LINKS: NavigationLink[] = [
+  {
+    href: '/',
+    label: 'Añadir',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/><path d="M12 5v14"/></svg>'
+  },
+  {
+    href: '/resumen',
+    label: 'Resumen',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>'
+  },
+  {
+    href: '/calendario',
+    label: 'Calendario',
+    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/><path d="M8 2v4"/><path d="M16 2v4"/></svg>'
+  }
+];
