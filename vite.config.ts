c361f96a9f62ade90ca829562936f3cc04015e36
Mario <bymaopa@local>
2026-04-19 20:26:54 +0200

    chore: update project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  build: {
+    target: 'esnext',
+    minify: 'esbuild'
+  },
+  optimizeDeps: {
+    include: ['svelte', '@tauri-apps/api', '@tauri-apps/plugin-fs']
+  }
 });
